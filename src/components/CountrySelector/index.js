d33b7ac9b48f152ce6745e45dbca98e9618e2be2
Khuong Mai <maingockhuong89@gmail.com>
2022-10-02 09:32:12 -0500

    Initial commit

---
+++ src/components/CountrySelector/index.js
@@ -0,0 +1,42 @@
+import {
+  FormControl,
+  InputLabel,
+  NativeSelect,
+  FormHelperText,
+  makeStyles,
+} from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: `${theme.spacing(3)}px 0`,
+  },
+}));
+
+export default function CountrySelector({ value, handleOnChange, countries }) {
+  const styles = useStyles();
+  return (
+    <FormControl className={styles.formControl}>
+      <InputLabel htmlFor="country-selector" shrink>
+        Country
+      </InputLabel>
+      <NativeSelect
+        value={value}
+        onChange={handleOnChange}
+        inputProps={{
+          name: "country",
+          id: "country-selector",
+        }}
+      >
+        {countries.map((country) => {
+          return (
+            <option key={country.ISO2} value={country.ISO2.toLowerCase()}>
+              {country.Country}
+            </option>
+          );
+        })}
+      </NativeSelect>
+      <FormHelperText>Select a country</FormHelperText>
+    </FormControl>
+  );
+}
